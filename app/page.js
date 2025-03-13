1e42054362d1c420fad11df1b34dc9266fee72cd
MahfujulSagor <me24020@mbstu.ac.bd>
2025-03-13 21:15:33 +0600

    add authentication

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import google from "@/public/google.svg";
 
 export default function Home() {
+  const {data: session} = useSession();
+  if (!session) {
+    return(
+      <div className="w-full min-h-screen flex items-center justify-center bg-black">
+        <button className="sm:text-2xl flex justify-center items-center px-8 py-3 bg-white gap-4 text-black font-medium rounded-xl cursor-pointer" onClick={()=> signIn("google")}><Image src={google} alt="google logo"/>Sign in with google</button>
+      </div>
+    )
+  }
   return (
-    <div>hello world!</div>
+    <div className="w-full min-h-screen flex items-center justify-center gap-8 bg-black">
+      <h1>Logged in {session?.user?.email}</h1>
+      <button className="bg-white text-2xl font-medium px-8 py-3 rounded-xl text-black cursor-pointer" onClick={()=> signOut()}>Logout</button>
+    </div>
   );
 }
--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 import { prisma } from "./utils/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
